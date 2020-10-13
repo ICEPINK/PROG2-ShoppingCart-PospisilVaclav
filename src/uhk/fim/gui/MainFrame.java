461b0751980ee437024ed89556ae948dd4556fb3
icepink <venayoda@gmail.com>
2020-10-13 17:27:42 +0200

    panel Main

--- src/uhk/fim/gui/MainFrame.java
+++ src/uhk/fim/gui/MainFrame.java
@@ -1,22 +1,31 @@
 package uhk.fim.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
 
-    public MainFrame() {
-        initFrame();
+    public MainFrame(int width,int height) {
+        super("PRO2 - Shopping cart");
+        setSize(width, height);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         initGUI();
     }
 
+    /* init bez Super
     public void initFrame() {
         setTitle("PRO2 - Shopping cart");
         setSize(800, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
+    */
 
     public void initGUI() {
+        JPanel panelMain = new JPanel(new BorderLayout());
+
+        add(panelMain);
 
     }
 }
